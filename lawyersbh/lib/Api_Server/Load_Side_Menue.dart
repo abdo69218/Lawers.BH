422c364c64da585cf55a3202e4250509274c422d
Abdo Gad <81652384+abdo69218@users.noreply.github.com>
2024-09-01 22:02:29 +0300

    Update Load_Side_Menue.dart

--- lawyersbh/lib/Api_Server/Load_Side_Menue.dart
+++ lawyersbh/lib/Api_Server/Load_Side_Menue.dart
@@ -5,7 +5,7 @@ class FetchApiSideMenue{
   Future<List<Load_Side_Menue_Model>> fetchsidemenue() async{
     List<Load_Side_Menue_Model> category = [];
 
-    http.Response response = await http.post('https://lawyers.bh/mobile_app/mobile_app_api/load_side_menu.php');
+    http.Response response = await http.post('');
     if (response.statusCode == 200 || response.statusCode == 400) {
       var body = jsonDecode(response.body);
 
